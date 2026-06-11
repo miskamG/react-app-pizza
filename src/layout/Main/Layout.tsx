84ed42f62d9a8c8762470e453c321880ea9e18e6
a.galeev <a.galeev@malltech.ru>
2026-06-11 13:45:48 +0500

    change search

--- src/layout/Main/Layout.tsx
+++ src/layout/Main/Layout.tsx
@@ -50,9 +50,9 @@ export function Layout() {
             }
           >
             <img src="./cart-icon.svg" alt="" />
-            Корзина
+            Корзина{' '}
+            <span className={styles.cartCount}>{items.reduce((acc, item) => acc + item.count, 0)}</span>
           </NavLink>
-          {items.reduce((acc, item) => acc + item.count, 0)}
         </div>
         <Button className={styles.exit} onClick={logout}>
           <img src="./exit-icon.svg" alt="" />
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,36 +1,30 @@
-import { useEffect, useState } from 'react'
+import axios, { AxiosError } from 'axios'
+import { useEffect, useState, type ChangeEvent } from 'react'
 import Headling from '../../components/Headling/Headling'
 import Search from '../../components/Search/Search'
 import { PREFIX } from '../../helpers/API'
 import type { Product } from '../../interfaces/Product.interface'
 import styles from './Menu.module.css'
-import axios, { AxiosError } from 'axios'
 import { MenuList } from './MenuList/MenuList'
 
 export default function Menu() {
   const [products, setProducts] = useState<Product[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | undefined>()
+  const [filter, setFilter] = useState<string>()
+  
+  useEffect(() => {
+    getMenu(filter)
+  }, [filter])
 
-  const getMenu = async () => {
-    // try {
-    //   const res = await fetch(`${PREFIX}/products`)
-
-    //   if (!res.ok) {
-    //     return
-    //   }
-
-    //   const data = (await res.json()) as Product[]
-
-    //   setProducts(data)
-    // } catch (error) {
-    //   console.error(error)
-
-    //   return
-    // }
+  const getMenu = async (name?: string) => {
     try {
       setIsLoading(true)
-      const { data } = await axios.get<Product[]>(`${PREFIX}/products`)
+      const { data } = await axios.get<Product[]>(`${PREFIX}/products`, {
+        params: {
+          name,
+        },
+      })
 
       setProducts(data)
       setIsLoading(false)
@@ -45,20 +39,24 @@ export default function Menu() {
     }
   }
 
-  useEffect(() => {
-    getMenu()
-  }, [])
+  const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value)
+  }
 
   return (
     <>
       <div className={styles.head}>
         <Headling>Меню</Headling>
-        <Search placeholder="Введите блюдо или состав" />
+        <Search
+          placeholder="Введите блюдо или состав"
+          onChange={updateFilter}
+        />
       </div>
       <div>
         {error && <p>{error}</p>}
-        {!isLoading && <MenuList products={products} />}
+        {!isLoading && products.length > 0 && <MenuList products={products} />}
         {isLoading && <p>Загрузка...</p>}
+        {!isLoading && products.length === 0 && <>Не найдено</>}
       </div>
     </>
   )
